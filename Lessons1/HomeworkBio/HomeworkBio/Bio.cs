648f2e9fb2164defdd7b41d1937d342f650ddb04
IgM <masik.igor.00@gmail.com>
2025-12-07 14:33:08 +0200

    Init commit

---
+++ Lessons1/HomeworkBio/HomeworkBio/Bio.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeworkBio
+{
+    internal class Bio
+    {
+        static readonly string[] bioData = {
+            "Улюблений фільм: Кримінальне чтиво (1994)",
+            "Улюблений мультфільм: Богиня благословляє цей чудовий світ (Kono Subarashii Sekai ni Shukufuku wo!)",
+            "Улюблена пісня: Disturbed - The Sound of Silence",
+            "Куплет з пісні:",
+            "   And in the naked light I saw",
+            "   Ten thousand people, maybe more",
+            "   People talking without speaking",
+            "   People hearing without listening",
+            "   People writing songs that voices never share",
+            "   And no one dare",
+            "   Disturb the sound of silence",
+            "   ------------------------------------------  ",
+            "   \"Fools\" said I, 'You do not know",
+            "   Silence like a cancer grows",
+            "   Hear my words that I might teach you",
+            "   Take my arms that I might reach you",
+            "   But my words like silent raindrops fell",
+            "   And echoed",
+            "   In the wells of silence",
+            "   ------------------------------------------  ",
+            "   And the people bowed and prayed",
+            "   To the neon god they made",
+            "   And the sign flashed out its warning",
+            "   In the words that it was forming",
+            "   And the sign said",
+            "   The words of the prophets",
+            "   are written on the subway walls",
+            "   and tenement halls",
+            "   and whisper'd in the sounds of silence",
+            "Співак:  Art Garfunkel",
+            "Автори пісні:  Paul Simon"
+        };
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            SetupConsoleColor();
+            Console.WriteLine("Bio Description:");
+
+            foreach (string line in bioData)
+                printBio(line, 10);
+
+            SetupConsole();
+        }
+
+        static void SetupConsoleColor()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+
+        static void SetupConsole()
+        {
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.Write("> ");
+            Console.CursorVisible = true;
+            Console.ReadLine();
+
+            Console.WindowWidth = 80;
+            Console.WindowHeight = 20;
+            Console.BufferWidth = Console.WindowWidth;
+            Console.BufferHeight = Console.WindowHeight;
+        }
+
+        static void printBio(string line, int delay = 100)
+        {
+            if (line.Contains("---"))
+                Console.WriteLine(line);
+            else
+                foreach (char c in line)
+                {
+                    Console.Write(c);
+                    Thread.Sleep(delay);
+                }
+                Console.WriteLine();
+        }
+    }
+}
---
+++ Lessons2/Lesson2FifteenGame3_12_2025/Lesson2FifteenGame3_12_2025/FifteenGame.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Lesson2FifteenGame3_12_2025
+{
+    class FifteenGame
+    {
+        static int[,] matrix = new int[4, 4];
+        static int emptyX = 3, emptyY = 3;
+
+        static void Main()
+        {
+            InitMatrix();
+            ShuffleMatrix();
+
+            while (true)
+            {
+                Console.Clear();
+                PrintMatrix();
+
+                if (Finish())
+                {
+                    Console.WriteLine("\nCongratulations, you win.");
+                    break;
+                }
+
+                InitHandleInput();
+            }
+        }
+
+        static void InitMatrix()
+        {
+            int v = 1;
+
+            for (int x = 0; x < 4; x++)
+                for (int y = 0; y < 4; y++)
+                    matrix[x, y] = v++;
+
+            matrix[3, 3] = 0;
+        }
+
+        private static void ShuffleMatrix()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 200; i++)
+            {
+                int dir = rnd.Next(4);
+                switch (dir)
+                {
+                    case 0:
+                        Move(-1, 0);
+                        break;
+                    case 1:
+                        Move(1, 0);
+                        break;
+                    case 2:
+                        Move(0, -1);
+                        break;
+                    case 3:
+                        Move(0, 1);
+                        break;
+                }
+            }
+        }
+
+        static void PrintMatrix()
+        {
+            Console.WriteLine("Game Fifteen\n");
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    Console.Write(((matrix[x, y] != 0) ? $"{matrix[x, y],3:d}" : "   "));
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void HandleInput(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    Move(1, 0);
+                    break;
+
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    Move(-1, 0);
+                    break;
+
+                case ConsoleKey.A:
+                case ConsoleKey.LeftArrow:
+                    Move(0, 1);
+                    break;
+
+                case ConsoleKey.D:
+                case ConsoleKey.RightArrow:
+                    Move(0, -1);
+                    break;
+            }
+        }
+
+        static void Move(int x, int y)
+        {
+            int nx = emptyX + x;
+            int ny = emptyY + y;
+
+            if (nx < 0 || nx > 3 || ny < 0 || ny > 3)
+            {
+                return;
+            }
+
+            matrix[emptyX, emptyY] = matrix[nx, ny];
+            matrix[nx, ny] = 0;
+
+            emptyX = nx;
+            emptyY = ny;
+        }
+
+        static void InitHandleInput()
+        {
+            var key = Console.ReadKey(true).Key;
+
+            HandleInput(key);
+        }
+
+        static bool Finish()
+        {
+            int v = 1;
+
+            for (int x = 0; x < 4; x++)
+                for (int y = 0; y < 4; y++)
+                {
+                    if (x == 3 && y == 3) return matrix[x, y] == 0;
+                    if (matrix[x, y] != v++) return false;
+                }
+
+            return true;
+        }
+    }
+
+}
+

5a38413a4444d11b437bf49ba6a010f47f73dc42
IgM <masik.igor.00@gmail.com>
2025-12-08 16:39:13 +0200

    calculate left work time

---
+++ Lessons2/WorkDayEnd/WorkDayEnd/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkDayEnd
+{
+    internal class Program
+    {
+        const int workDayHours = 8;
+        const double secondsInOneHour = 3600;
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введіть скільки пройшло секунд з початку робочого дня :");
+            Console.Write("1 година рівна 3600 секунд: ");
+            Int32 seccondsPassed = Convert.ToInt32(Console.ReadLine());
+
+            double totalSeccodsPerDay = ConvertHoursToSeconds(workDayHours, secondsInOneHour);
+            double remainingSeconds = CalculeteRemainingSeconds(totalSeccodsPerDay, seccondsPassed);
+
+            CalculateTimeLeft(remainingSeconds);
+            
+        }
+
+        static double ConvertHoursToSeconds(int hours, double seconds)
+        {
+            return Convert.ToDouble(hours * seconds);
+        }
+
+        static double CalculeteRemainingSeconds(double totalSeconds, double passedSeconds)
+        {
+            return totalSeconds - passedSeconds;
+        }
+
+        static int SeccondsToHours(double seconds)
+        {
+            return Convert.ToInt16(seconds / secondsInOneHour);
+        }
+
+        static int SeccondsToMinutes(double seconds)
+        {
+            return Convert.ToInt16((seconds % secondsInOneHour) / 60);
+        }
+
+        static int GetRemainingSeconds(double seconds)
+        {
+            return Convert.ToInt16((seconds % secondsInOneHour) % 60);
+        }
+
+        static void PrintMessage(int[] hourLeft)
+        {
+            if (hourLeft[0] <= 0 && hourLeft[1] <= 0 && hourLeft[2] <= 0)
+            {
+                Console.WriteLine("Робочий день вже закінчився!");
+                return;
+            }
+
+            Console.WriteLine($"Залишок робочого дня: {hourLeft[0]} годин, {hourLeft[1]} хвилин, {hourLeft[2]} секунд.");
+        }
+
+        static void CalculateTimeLeft(double totalSecondsLeft)
+        {
+            int hours = SeccondsToHours(totalSecondsLeft);
+            int minutes = SeccondsToMinutes(totalSecondsLeft);
+            int seconds = GetRemainingSeconds(totalSecondsLeft);
+
+            PrintMessage(new int[] { hours, minutes, seconds });
+        }
+    }
+}
---
+++ Lessons2/WorkDayEnd/WorkDayEnd/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("WorkDayEnd")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WorkDayEnd")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ec5fe1e2-cfaf-409c-a348-6d6a70687f4c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

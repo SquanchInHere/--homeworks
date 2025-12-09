de0490b316eaa296d8c313060ced4c270352566a
IgM <masik.igor.00@gmail.com>
2025-12-10 00:52:46 +0200

    add convert summ

---
+++ Lessons3/CorrectMoney/CorrectMoney/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace CorrectMoney
+{
+    internal class Program
+    {
+        const int COEFFICIENT = 100;
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int hryvnia = SetHryvnia();
+            int kopiyka = SetKopiyka();
+
+            CalculateSum(hryvnia, kopiyka);
+        }
+
+        static int SetHryvnia()
+        {
+            int hryvnia = 0;
+
+            while (true)
+            {
+                Console.Write("Введіть сумму в (грн): ");
+
+                if (!int.TryParse(Console.ReadLine(), out hryvnia) && hryvnia < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Некоректне введенно значення!");
+                    Console.ResetColor();
+                }
+                else
+                    return hryvnia;
+            }
+        }
+
+        static int SetKopiyka()
+        {
+            int kopiyka = 0;
+
+            while (true)
+            {
+                Console.Write("Введіть сумму в (коп): ");
+
+                if (!int.TryParse(Console.ReadLine(), out kopiyka) && kopiyka < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Некоректне введенно значення!");
+                    Console.ResetColor();
+                }
+                else
+                    return kopiyka;
+            }
+        }
+
+        static void CalculateSum(int hryvnia , int kopiyka)
+        {
+            int total = hryvnia * COEFFICIENT + kopiyka;
+            int resultHryvnia = total / COEFFICIENT;
+            int resultKopiyka = total % COEFFICIENT;
+
+            Console.WriteLine($"Ваша сумма дорівнює: {resultHryvnia} грн {resultKopiyka} коп.");
+        }
+    }
+}
